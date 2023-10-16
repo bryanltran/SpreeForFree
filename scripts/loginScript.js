3a688344f1ab645101b317eb79905b5a6d00d6fb
Jaiden Callies <Jaiden.N.Callies-1@ou.edu>
2023-10-16 00:52:21 -0500

    Add functionality to retrieve user data from html files to two functions that will handle account creation and login attempts

--- scripts/loginScript.js
+++ scripts/loginScript.js
@@ -1,4 +1,16 @@
 function attemptLogin()
 {
+    // Retrieve user data
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+}
 
+function createAccount()
+{
+    // Retrieve user data
+    var first_name = document.getElementById("firstname").value;
+    var last_name = document.getElementById("lastname").value;
+    var username_CA = document.getElementById("username_CA").value;
+    var password_CA = document.getElementById("password_CA").value;
+    var password_confirm = document.getElementById("passwordConfirm").value;
 }
