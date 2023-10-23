be884bb540d2e0d216b75140cf9ab10a76c77cb3
Jaiden Callies <Jaiden.N.Callies-1@ou.edu>
2023-10-23 02:14:05 -0500

    Add login/account to the navigation bar and update page transitions

--- scripts/loginScript.js
+++ scripts/loginScript.js
@@ -8,6 +8,7 @@ function attemptLogin()
     if (JSON.parse(localStorage.getItem(username)) === null)
     {
         alert("Invalid username. Please try again.");
+        return true;
     }
     // Username is valid, check if password matches the username
     else 
@@ -15,12 +16,20 @@ function attemptLogin()
         // Entered successful pair of information
         if (JSON.parse(localStorage.getItem(username)).pw === password)
         {
-            alert("Login successful!");
+            sessionStorage.clear();
+            sessionStorage.setItem("Username", username);
+            sessionStorage.setItem("First Name", JSON.parse(localStorage.getItem(username)).fn);
+            sessionStorage.setItem("Last Name", JSON.parse(localStorage.getItem(username)).ln);
+            sessionStorage.setItem("Tokens", JSON.parse(localStorage.getItem(username)).tokens);
+            alert("Welcome!");
+            window.location = "../pages/userHome.html";
+            return false;
         }
         // Invalid password entered
         else
         {
             alert("Invalid password. Please try again.");
+            return true;
         }
     }
 }
@@ -41,19 +50,84 @@ function createAccount()
         if (password_CA === password_confirm)
         {
             // Save user data to local storage
-            var userData = {pw: password_CA, fn: first_name, ln: last_name};
+            var userData = {pw: password_CA, fn: first_name, ln: last_name, tokens: 100};
             localStorage.setItem(username_CA, JSON.stringify(userData));
             alert("Account successfully created!");
+            window.location = "../pages/login.html";
+            return false;
         }
         // Passwords do not match
         else
         {
             alert("Passwords do not match. Please try again.");
+            return true;
         }
     }
     // Username is unavailable
     else
     {
         alert("The username is already taken. Please try again.");
+        return true;
     }
+}
+
+function checkIsLoggedIn(pageId)
+{
+    // Check which page is being accessed
+    if (pageId === "homeNavBar") 
+    {
+        // User is not logged in
+        if (sessionStorage.getItem("Username") == null)
+        {
+            window.location = "../index.html";
+            return false;
+        }
+        // User is logged in
+        else
+        {
+            window.location = "userHome.html";
+            return false;
+        }
+    }
+    // Check which page is being accessed
+    else if (pageId === "accountNavBar")
+    {
+        // User is not logged in
+        if (sessionStorage.getItem("Username") == null)
+        {
+            window.location = "login.html";
+            return false;
+        }
+        // User is logged in
+        else
+        {
+            window.location = "account.html";
+            return false;
+        }
+    }
+}
+
+function outputAccountInfo()
+{
+    var p1 = document.createElement('p');
+    var p2 = document.createElement('p');
+    p1.innerText="Welcome " + sessionStorage.getItem("First Name") + " " + sessionStorage.getItem("Last Name") + "!";
+    p2.innerText="Current Number of Tokens: " + sessionStorage.getItem("Tokens");
+    document.getElementById("accountInfo").appendChild(p1);
+    document.getElementById("accountInfo").appendChild(p2);
+}
+
+function attemptLogOut()
+{
+    sessionStorage.clear();
+    window.location = "../index.html";
+    return false;
+}
+
+function deleteAccount()
+{
+    localStorage.removeItem(sessionStorage.getItem("Username"));
+    sessionStorage.clear();
+    window.location = "../index.html";
+    return false;
 }
