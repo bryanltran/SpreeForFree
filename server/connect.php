575a0fe5e3a507dbc00442ad16a1c20aab7c3aff
bryanltran <bryan.l.tran-1@ou.edu>
2023-10-30 22:48:46 -0500

    Remove unneccesary files

--- server/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $firstName=$_Post['firstname'];
-    $lastName=$_Post['lastname'];
-    $username=$_Post['username'];
-    $password=$_Post['password'];
-
-    //Database connection
-    
-?>
