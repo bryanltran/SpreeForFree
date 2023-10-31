b3f9ec8484a50e75f1abd2d170f5525edbfd2d92
bryanltran <bryan.l.tran-1@ou.edu>
2023-10-30 20:40:59 -0500

    Add user login information connection to database

---
+++ server/connect.php
@@ -0,0 +1,9 @@
+<?php
+    $firstName=$_Post['firstname'];
+    $lastName=$_Post['lastname'];
+    $username=$_Post['username'];
+    $password=$_Post['password'];
+
+    //Database connection
+    
+?>
