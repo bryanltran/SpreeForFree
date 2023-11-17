e32a78bbe1c7d7d3f7464464ec58d54ee4db994d
bryanltran <bryan.l.tran-1@ou.edu>
2023-11-16 18:44:40 -0600

    Create basic tests

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,33 @@
 console.log("Running test..");
 
+document.addEventListener('DOMContentLoaded', () => {
+    
+    
+    describe('Automated Website Tests', () => {
+    
+        it('Page should have a title', () => {
+        expect(document.title).toBeTruthy();
+        });
+
+        it('Page should have the Navigation Bar', () => {
+        const navMenu = document.getElementById('navbar');
+        expect(navMenu).toBeTruthy();
+        });
+
+        it('should display an alert when any button is clicked', () => {
+        // Get all buttons on the page
+        const buttons = document.querySelectorAll('button');
+
+        // Iterate through each button and simulate a click
+        buttons.forEach(button => {
+            button.click();
+            // Test if the window.alert function has been called
+            expect(window.alert).toHaveBeenCalled();
+        });
+        });
+    
+    });
+  
+  });
+  
 console.log("Test completed.");
