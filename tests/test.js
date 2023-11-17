4b347c231970de06b069f7097bd05bdc86c28704
bryanltran <bryan.l.tran-1@ou.edu>
2023-11-16 19:00:52 -0600

    Create tests for title and navbar

--- tests/test.js
+++ tests/test.js
@@ -1,21 +1,31 @@
+const puppeteer = require('puppeteer');
+
 console.log("Running test..");
 
-document.addEventListener('DOMContentLoaded', () => {
-    
-    
-    describe('Automated Website Tests', () => {
-    
-        it('Page should have a title', () => {
-        expect(document.title).toBeTruthy();
-        });
+describe('Automated Website Tests', () => {
+    let browser;
+    let page;
 
-        it('Page should have the Navigation Bar', () => {
-        const navMenu = document.getElementById('navbar');
-        expect(navMenu).toBeTruthy();
-        });
-    
+    beforeAll(async () => {
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
     });
-  
-  });
-  
+
+    afterAll(async () => {
+        await browser.close();
+    });
+
+    it('Page should have a title', async () => {
+        await page.goto('https://your-website-url');
+        const title = await page.title();
+        expect(title).toBeTruthy();
+    });
+
+    it('Page should have the Navigation Bar', async () => {
+        await page.goto('https://your-website-url');
+        const navMenu = await page.$('#navbar');
+        expect(navMenu).toBeTruthy();
+    });
+});
+
 console.log("Test completed.");
