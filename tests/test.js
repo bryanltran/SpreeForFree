bd4e8a4be5e78e151a869309ee0ce72a639ae529
bryanltran <bryan.l.tran-1@ou.edu>
2023-11-16 18:46:35 -0600

    Remove test for alert buttons

--- tests/test.js
+++ tests/test.js
@@ -13,18 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
         const navMenu = document.getElementById('navbar');
         expect(navMenu).toBeTruthy();
         });
-
-        it('should display an alert when any button is clicked', () => {
-        // Get all buttons on the page
-        const buttons = document.querySelectorAll('button');
-
-        // Iterate through each button and simulate a click
-        buttons.forEach(button => {
-            button.click();
-            // Test if the window.alert function has been called
-            expect(window.alert).toHaveBeenCalled();
-        });
-        });
     
     });
   
