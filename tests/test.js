b7f18a63a82fe52b081701d9e6e6a3911a369fc9
bryanltran <bryan.l.tran-1@ou.edu>
2023-11-21 00:10:55 -0600

    Create Tests for Title and Nav

--- tests/test.js
+++ tests/test.js
@@ -1,13 +1,46 @@
 const { JSDOM } = require('jsdom');
 
-console.log("Running test..");
-test('Page has a title', () => {
-  const html = '<!DOCTYPE html><html><head><title>My Page Title</title></head><body></body></html>';
+const { JSDOM } = require('jsdom');
+
+console.log("Running tests..");
+
+// Test 1: Home Page has a title
+test('Home Page has a title', () => {
+  const html = '<!DOCTYPE html><html><head><title>Spree For Free</title></head><body></body></html>';
   const dom = new JSDOM(html);
   const titleElement = dom.window.document.querySelector('title');
 
   expect(titleElement).not.toBeNull();
 });
+
+// Test 2: Home Page has a navigation menu
+test('Home Page has a navigation menu', () => {
+  const html = '<!DOCTYPE html><html><head></head><body><nav><ul><li>Home</li><li>Catalog</li><li>About</li><li>Account</li><li>Cart</li></ul></nav></body></html>';
+  const dom = new JSDOM(html);
+  const navElement = dom.window.document.querySelector('nav');
+
+  expect(navElement).not.toBeNull();
+});
+
+// Test 3: Catalog Page has a title
+test('Catalog Page has a title', () => {
+  const html = '<!DOCTYPE html><html><head><title>Catalog</title></head><body></body></html>';
+  const dom = new JSDOM(html);
+  const titleElement = dom.window.document.querySelector('title');
+
+  expect(titleElement).not.toBeNull();
+});
+
+// Test 4: Catalog Page has a navigation menu
+test('Catalog Page has a navigation menu', () => {
+  const html = '<!DOCTYPE html><html><head></head><body><nav><ul><li>Home</li><li>Catalog</li><li>About</li><li>Account</li><li>Cart</li></ul></nav></body></html>';
+  const dom = new JSDOM(html);
+  const navElement = dom.window.document.querySelector('nav');
+
+  expect(navElement).not.toBeNull();
+});
+
+console.log("Tests completed.");
 
 
 console.log("Test completed.");
